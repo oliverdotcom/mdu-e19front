ceff70702f0c86ac263265003fee70fbed32adf3
Rasmus Cederdorff <rasmus@cederdorff.com>
2020-08-27 21:15:41 +0200

    Update main.js

--- spa-products-template/js/main.js
+++ spa-products-template/js/main.js
@@ -2,18 +2,30 @@
 
 // =========== Product functionality =========== //
 
+/**
+ * Array of product objects
+ */
 let products = [
   // to do
 ];
 
+/**
+ * Appending products to the DOM using a for-of loop
+ */
 function appendProducts() {
   // to do
 }
 
+/**
+ * Adding a new product to the Array of products
+ */
 function addNewProduct() {
   // to do
 }
 
+/**
+ * Searching for products matching the input value
+ */
 function search(value) {
   // to do
 }
