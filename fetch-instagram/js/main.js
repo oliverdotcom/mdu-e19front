f794441df2ca83503d15e44be16e1287874ebb7a
Rasmus Cederdorff <rasmus@cederdorff.com>
2020-09-03 08:55:19 +0200

    Comments added

--- fetch-instagram/js/main.js
+++ fetch-instagram/js/main.js
@@ -2,8 +2,8 @@
 
 async function fetchInstagramPosts(userName) {
   let url = `https://instagram.com/${userName}/?__a=1`;
-  let response = await fetch(url);
-  let data = await response.json();
+  let response = await fetch(url); // fetch and wait the response
+  let data = await response.json(); // read response body and wait for parsing the JSON
   let posts = data.graphql.user.edge_owner_to_timeline_media.edges;
   appendPosts(posts);
 }
