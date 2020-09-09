6577fd85a7569182ca5bd9ee73da6221605a08a4
Rasmus Cederdorff <rasmus@cederdorff.com>
2020-09-09 10:20:00 +0200

    Update main.js

--- spa-headless-cms-movies-class-structure-template/scripts/main.js
+++ spa-headless-cms-movies-class-structure-template/scripts/main.js
@@ -1,19 +1,14 @@
-import Spa from "./spa.js";
-import MovieService from "./movie-service.js";
 
-let spa = new Spa("movies");
-let movieService = new MovieService();
-movieService.getMovies();
 
 //functions in global variables
-window.pageChange = function() {
+window.pageChange = function () {
 
 }
 
-window.categorySelected = function(id) {
+window.categorySelected = function (id) {
 
 }
 
-window.search = function(searchValue) {
+window.search = function (searchValue) {
 
 }
